be08f2e83c3e40885baab55bd2c6352dabd27e8a
Jamie Lemon <lemon.jamie@gmail.com>
2017-08-02 14:18:22 -0400

    Created the Timeslot, Restriction, Schedule, Section and Student classes

---
+++ FinalProject/Models/Timeslot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Models
+{
+    public class Timeslot
+    {
+        public string dayOfWeek { get; set; }
+        public int startTime { get; set; }
+        public int duration { get; set; }
+    }
+}
