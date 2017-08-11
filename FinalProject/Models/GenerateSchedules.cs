ae99fd1c09eda63f83190365302e1e3ee8d993f1
Joao <jpedrosffair@gmail.com>
2017-08-11 09:23:12 -0400

    renaming and small changes

---
+++ FinalProject/Models/GenerateSchedules.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace FinalProject.Models
+{
+    public class GenerateSchedules
+    {
+        public Restriction Restriction { get; set; }
+        public List<Section> Sections { get; set; }
+
+        public List<Schedule> Schedules { get; set; }
+
+        
+    }
+
+}
